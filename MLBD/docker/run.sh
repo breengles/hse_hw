09a937bea25fb723b5e20202f1512a71274e1a80
Artem Kotov <breengles@gmail.com>
2021-02-08 01:46:37 +0300

    fixed personal ports

--- MLBD/docker/run.sh
+++ MLBD/docker/run.sh
@@ -3,11 +3,13 @@ docker run \
 	--privileged=true \
 	-t -i \
 	--publish-all=true \
-	-p 8888:8888 \
-	-p 9999:9999 \
-	-p 8088:8088 \
-	-p 7180:7180 \
-	-p 80:80 \
+	-p 8887:8887 \
+	-p 9987:9987 \
+	-p 8087:8088 \
 	-v $(pwd)/..:/workspace \
+    -v /home/breengles/mlbd_datasets:/workspace/data \
 	ishugaepov/mlbd \
-	/usr/bin/docker-quickstart
+	/usr/bin/docker-quickstart \
+
+	# -p 7187:7187 \
+	# -p 87:87 \
