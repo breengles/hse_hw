1e27200e491f5fedf9e8d096798d17667c5944c0
Artem Kotov <breengles@gmail.com>
2021-03-02 20:27:35 +0300

    prep to run experiments

--- ReinforcementLearning/03. Walker2D/train.py
+++ ReinforcementLearning/03. Walker2D/train.py
@@ -14,32 +14,18 @@ import pandas as pd
 
 ENV_NAME = "Walker2DBulletEnv-v0"
 
-LAMBDA = 0.95
-GAMMA = 0.99
-
-ACTOR_LR = 2e-4
-CRITIC_LR = 1e-4
-
-CLIP = 0.2
-ENTROPY_COEF = 1e-1
-BATCHES_PER_UPDATE = 64
-BATCH_SIZE = 64
-
 MIN_TRANSITIONS_PER_UPDATE = 2048
 MIN_EPISODES_PER_UPDATE = 4
-
 ITERATIONS = 10000
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-    
-def compute_lambda_returns_and_gae(trajectory):
+def compute_lambda_returns_and_gae(trajectory, lam=0.97, gamma=0.99):
     lambda_returns = []
     gae = []
     last_lr = 0.
     last_v = 0.
     for _, _, r, _, v in reversed(trajectory):
-        ret = r + GAMMA * (last_v * (1 - LAMBDA) + last_lr * LAMBDA)
+        ret = r + gamma * (last_v * (1 - lam) + last_lr * lam)
         last_lr = ret
         last_v = v
         lambda_returns.append(last_lr)
@@ -95,14 +81,15 @@ class Critic(nn.Module):
 
 
 class PPO:
-    def __init__(self, state_dim, action_dim):
-        self.actor = Actor(state_dim, action_dim).to(DEVICE)
-        self.critic = Critic(state_dim).to(DEVICE)
-        self.actor_optim = Adam(self.actor.parameters(), ACTOR_LR)
-        self.critic_optim = Adam(self.critic.parameters(), CRITIC_LR)
+    def __init__(self, state_dim, action_dim, actor_lr=3e-4, critic_lr=2e-4):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.actor = Actor(state_dim, action_dim).to(self.device)
+        self.critic = Critic(state_dim).to(self.device)
+        self.actor_optim = Adam(self.actor.parameters(), actor_lr)
+        self.critic_optim = Adam(self.critic.parameters(), critic_lr)
 
-    def update(self, trajectories):
-        transitions = [t for traj in trajectories for t in traj] # Turn a list of trajectories into list of transitions
+    def update(self, trajectories, clip=0.2, entropy_coef=1e-2, batches_per_update=64, batch_size=64):
+        transitions = [t for traj in trajectories for t in traj]  # Turn a list of trajectories into list of transitions
         state, action, old_prob, target_value, advantage = zip(*transitions)
         state = np.array(state)
         action = np.array(action)
@@ -112,42 +99,41 @@ class PPO:
         advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
         
         
-        for _ in range(BATCHES_PER_UPDATE):
-            idx = np.random.randint(0, len(transitions), BATCH_SIZE)  # Choose random batch
-            s = torch.tensor(state[idx]).float().to(DEVICE)
-            a = torch.tensor(action[idx]).float().to(DEVICE)
-            op = torch.tensor(old_prob[idx]).float().to(DEVICE)  # Probability of the action in state s.t. old policy
-            v = torch.tensor(target_value[idx]).float().to(DEVICE)  # Estimated by lambda-returns 
-            adv = torch.tensor(advantage[idx]).float().to(DEVICE)  # Estimated by generalized advantage estimation 
+        for _ in range(batches_per_update):
+            idx = np.random.randint(0, len(transitions), batch_size)  # Choose random batch
+            s = torch.tensor(state[idx]).float().to(self.device)
+            a = torch.tensor(action[idx]).float().to(self.device)
+            op = torch.tensor(old_prob[idx]).float().to(self.device)  # Probability of the action in state s.t. old policy
+            v = torch.tensor(target_value[idx]).float().to(self.device)  # Estimated by lambda-returns 
+            adv = torch.tensor(advantage[idx]).float().to(self.device)  # Estimated by generalized advantage estimation 
             
             # TODO: Update actor here
             prob_new, distr = self.actor.compute_proba(s, a)
             entropy = distr.entropy().mean()
             ratio = prob_new / op
-            actor_loss = -torch.mean(torch.minimum(ratio * adv, torch.clip(ratio, 1 - CLIP, 1 + CLIP) * adv))
-            actor_loss -= ENTROPY_COEF * entropy
-            self.actor_optim.zero_grad()            
+            actor_loss = -torch.mean(torch.minimum(ratio * adv, torch.clip(ratio, 1 - clip, 1 + clip) * adv))
+            actor_loss -= entropy_coef * entropy
+            self.actor_optim.zero_grad()
             actor_loss.backward()
             self.actor_optim.step()
             
             # TODO: Update critic here
             val = self.critic.get_value(s).flatten()
             critic_loss = F.mse_loss(val, v)
+            # critic_loss = F.smooth_l1_loss(val, v)
             self.critic_optim.zero_grad()
             critic_loss.backward()
             self.critic_optim.step()
             
-            
-            
     def get_value(self, state):
         with torch.no_grad():
-            state = torch.tensor(np.array([state])).float().to(DEVICE)
+            state = torch.tensor(np.array([state])).float().to(self.device)
             value = self.critic.get_value(state)
         return value.cpu().item()
 
     def act(self, state):
         with torch.no_grad():
-            state = torch.tensor(np.array([state])).float().to(DEVICE)
+            state = torch.tensor(np.array([state])).float().to(self.device)
             action, pure_action, distr = self.actor.act(state)
             prob = torch.exp(distr.log_prob(pure_action).sum(-1))
         return action.cpu().numpy()[0], pure_action.cpu().numpy()[0], prob.cpu().item()
@@ -170,7 +156,7 @@ def evaluate_policy(env, agent, episodes=5):
     return returns
    
 
-def sample_episode(env, agent):
+def sample_episode(env, agent, lam=0.97, gamma=0.99):
     s = env.reset()
     d = False
     trajectory = []
@@ -180,7 +166,7 @@ def sample_episode(env, agent):
         ns, r, d, _ = env.step(a)
         trajectory.append((s, pa, r, p, v))
         s = ns
-    return compute_lambda_returns_and_gae(trajectory)
+    return compute_lambda_returns_and_gae(trajectory, lam=lam, gamma=gamma)
 
 if __name__ == "__main__":
     env = make(ENV_NAME)
