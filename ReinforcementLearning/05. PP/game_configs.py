14e9bb0916f269c5c1810657c193c2fbd66b22ef
Artem Kotov <breengles@gmail.com>
2021-06-04 20:30:08 +0300

    added config for 4x4 field

--- ReinforcementLearning/05. PP/game_configs.py
+++ ReinforcementLearning/05. PP/game_configs.py
@@ -40,3 +40,23 @@ simple = {
     }
 }
 
+simplier = {
+    "game": {
+        "num_obsts": 0,
+        "num_preds": 1,
+        "num_preys": 1,
+        "x_limit": 4,
+        "y_limit": 4,
+        "obstacle_radius_bounds": [0.8, 2.0],
+        "prey_radius": 0.8,
+        "predator_radius": 1.0,
+        "predator_speed": 6.0,
+        "prey_speed": 9.0,
+        "world_timestep": 1/100,
+        "frameskip": 5
+    },
+    "environment": {
+        "frameskip": 5,
+        "time_limit": 1000
+    }
+}
