eb8cd7338f995cfe15e9538ec78bb101ddb42864
Artem Kotov <breengles@gmail.com>
2021-10-19 01:00:01 +0300

    first iteration of WARP

--- RecSys/01/factorization.py
+++ RecSys/01/factorization.py
@@ -30,16 +30,16 @@ class MatrixFactorization:
         self.U = None
         self.I = None
 
-    def predict_user(self, user_id):
-        return self.I @ self.U[user_id].T
+    def predict_user(self, user):
+        return self.I @ self.U[user].T
 
     def similar_items(self, item_id, n=10):
         sims = cosine_similarity(self.I)[item_id]
         return np.argsort(sims)[-(n + 1) : -1][::-1]
 
-    def recommend(self, user_id, n=10):
-        already_liked = set(self.R.getrow(user_id).indices)
-        rec = np.argsort(self.predict_user(user_id))[::-1]
+    def recommend(self, user, n=10):
+        already_liked = set(self.R.getrow(user).indices)
+        rec = np.argsort(self.predict_user(user))[::-1]
         return list(islice((r for r in rec if r not in already_liked), n))
 
     def _init_matrices(self, R):
@@ -161,12 +161,12 @@ class BPR(MatrixFactorization):
 
         sigmoid = np.tile(expit(-r_uij), (self.factors, 1)).T
 
-        grad_u = sigmoid * (item_j - item_i) + self.weight_decay * user_u
-        grad_i = -sigmoid * user_u + self.weight_decay * item_i
-        grad_j = sigmoid * user_u + self.weight_decay * item_j
-        self.U[u] -= self.lr * grad_u
-        self.I[i] -= self.lr * grad_i
-        self.I[j] -= self.lr * grad_j
+        upd_u = sigmoid * (item_j - item_i) + self.weight_decay * user_u
+        upd_i = -sigmoid * user_u + self.weight_decay * item_i
+        upd_j = sigmoid * user_u + self.weight_decay * item_j
+        self.U[u] -= self.lr * upd_u
+        self.I[i] -= self.lr * upd_i
+        self.I[j] -= self.lr * upd_j
 
     def __sample(self, n_users, n_items, batch_size, indices, indptr):
         sampled_pos_items = np.zeros(batch_size, dtype=int)
@@ -208,5 +208,81 @@ class BPR(MatrixFactorization):
 
                 if self.verbose:
                     print(f"Iter: {it + 1: 6d} | AUC: {auc:0.5f} | AUC2: {auc2:0.5f}")
+
+        return self
+
+
+class WARP(MatrixFactorization):
+    def __sample(self):
+        n_users, n_items = self.R.shape
+
+        user = np.random.choice(n_users)
+        items_pos = self.R[user].nonzero()[1]
+
+        if items_pos.shape[0] == 0:
+            return
+
+        items_neg = np.arange(n_items)[
+            ~np.isin(np.arange(n_items), items_pos)
+        ]  # kinda trick to extract negative items?
+        np.random.shuffle(items_neg)
+
+        item_pos = np.random.choice(items_pos)
+        score_pos = self.U[user] @ self.I[item_pos]
+
+        for trial, item_neg in enumerate(items_neg, start=1):
+            score_neg = self.U[user] @ self.I[item_neg]
+            margin = 1 - score_pos + score_neg
+            if margin > 0:
+                self.__num_of_trials += trial
+                return user, item_pos, item_neg, trial, items_neg.shape[0]
+
+            self.__correct_cnt += score_pos > score_neg
+
+        # in case we haven't found any violating negative item
+        # we still need to update number of trials made
+        self.__num_of_trials += trial
+
+    def __step(self):
+        self.__correct_cnt = self.__num_of_trials = 0
+        for _ in range(self.R.shape[0]):
+            sample = self.__sample()
+            if sample is None:
+                continue
+
+            user, item_pos, item_neg, num_of_trials, items_neg_total = sample
+            loss = np.log(np.floor(items_neg_total / num_of_trials))
+
+            upd_user = self.lr * (loss * (self.I[item_neg] - self.I[item_pos]) + self.weight_decay * self.U[user])
+            upd_pos = self.lr * (-loss * self.U[user] + self.weight_decay * self.I[item_pos])
+            upd_neg = self.lr * (loss * self.U[user] + self.weight_decay * self.I[item_neg])
+
+            if num_of_trials == 1:
+                upd_user *= 10
+                upd_pos *= 10
+                upd_neg *= 10
+
+            self.U[user] -= upd_user
+            self.I[item_pos] -= upd_pos
+            self.I[item_neg] -= upd_neg
+
+        return self.__correct_cnt / self.__num_of_trials
+
+    def fit(self, R):
+        self._init_matrices(R)
+
+        for it in range(self.iterations):
+            acc = self.__step()
+
+            if (it + 1) % self.save_every == 0:
+                auc, auc2 = roc_auc(R, self)
+
+                self.logger.log("iter", it + 1)
+                self.logger.log("AUC", auc)
+                self.logger.log("AUC2", auc2)
+                self.logger.log("acc", acc)
+
+                if self.verbose:
+                    print(f"Iter: {it + 1: 6d} | AUC: {auc:0.5f} | AUC2: {auc2:0.5f} | ACC: {acc:0.5f}")
 
         return self
