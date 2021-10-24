bbe836618a6b4ee3e44e8dbb14c6774a0efee6fc
Artem Kotov <breengles@gmail.com>
2021-10-24 19:46:52 +0300

    cleanup

--- ProdStories/05. Vacuum/wrapper.py
+++ ProdStories/05. Vacuum/wrapper.py
@@ -2,29 +2,33 @@ from mapgen import Dungeon
 
 
 class Wrapper(Dungeon):
-    def __init__(self, *args, timepenalty=False, **kwargs):
+    def __init__(self, *args, timepenalty=False, transpose=False, **kwargs):
         super().__init__(*args, **kwargs)
         self.timepenalty = timepenalty
+        self.transpose = transpose
 
     def reset(self):
         observation = super().reset()
-        return observation[:, :, :-1].transpose(2, 0, 1)  # as torch conv wotks on (C, H, W)
+        return observation.transpose(2, 0, 1) if self.transpose else observation
+        # return observation[:, :, :-1].transpose(2, 0, 1)  # as torch conv wotks on (C, H, W)
 
     def step(self, action: int):
-        observation, metric, done, info = super().step(action)
-        observation = observation[:, :, :-1]
+        observation, reward, done, info = super().step(action)
 
-        reward = 0
+        if self.transpose:
+            observation = observation.transpose(2, 0, 1)
+            # observation = observation[:, :, :-1].transpose(2, 0, 1)
+
         if info["is_new"]:
             reward += 1
 
         if self.timepenalty:
             reward -= 0.1
 
-        if info["collided"]:
-            reward -= 0.5
+        # if info["collided"]:
+        #     reward -= 0.5
 
         # if not info["moved"]:
         #     reward -= 1
 
-        return observation.transpose(2, 0, 1), reward, done, metric, info
+        return observation, reward, done, info
